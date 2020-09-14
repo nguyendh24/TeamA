521ff352f350d5a43b8a2186233cdf015dffa603
Diana Nguyen <>
2020-09-14 13:25:47 -0400

    Android Studio code base

---
+++ app/src/main/java/com/example/teama/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.teama;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    /**
+     * Defining all the elements of layout
+     */
+
+    private EditText eEmail;
+    private EditText ePassword;
+    private TextView eAttempts;
+    private Button eLogin;
+    private TextView eNewUser;
+    private int counter = 5;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eEmail = findViewById(R.id.editTextEmailAddress); //findViewById connects/binds to respective xml layout to variable
+        ePassword = findViewById(R.id.editTextRegPW);
+        eAttempts = findViewById(R.id.textViewAttempts);
+        eLogin = findViewById(R.id.buttonLogin);
+        eNewUser = findViewById(R.id.textViewSignUp);
+
+        eNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  startActivity(new Intent(LoginActivity.this, Registration.class));
+            }
+        });
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputEmail = eEmail.getText()); //need toString to convert eEmail
+                String inputPW = ePassword.getText().toString();
+
+                if (inputEmail.isEmpty() || inputPW.isEmpty()) { //valid input
+                    Toast.makeText(LoginActivity.this, "invalid input ",Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!isValidEmail(inputEmail)) {
+                        counter--;
+                        eAttempts.setText("# of attempts remaining: " + counter);
+                        Toast.makeText(LoginActivity.this, "invalid email ", Toast.LENGTH_SHORT).show();
+                        if (counter == 0)
+                            eLogin.setEnabled(false); //if user exceed login attempts button will be disabled
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login success! ",Toast.LENGTH_SHORT).show();
+                        /*******
+                         * CODE AFTER LOGIN SCREEN
+                         */
+                        //Intent intent = new Intent(MainActivity.this, "NextPageAfterLoginGoesHere.class");
+                        //startActivity(intent);
+
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Validating appropriate Email -- using Patterns &TextUtil
+     * @param target
+     * @return
+     */
+    private boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+}
