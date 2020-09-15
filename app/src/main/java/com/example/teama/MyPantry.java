64420283cf0d1d9d30794cb1635c58b2150b2e25
DESKTOP-IK851V1\dariu <robertsond3@vcu.edu>
2020-09-15 00:30:17 -0400

    Added action to the login button. Added blank settings page and blank pantry page. added buttons on main activity page. The two named buttons have actions to go to different pages that are empty.

---
+++ app/src/main/java/com/example/teama/MyPantry.java
@@ -0,0 +1,14 @@
+package com.example.teama;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPantry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_pantry);
+    }
+}
