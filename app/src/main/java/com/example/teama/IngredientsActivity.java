02cdb8929c0e348d24f15c8e3ea09a084af12580
DESKTOP-IK851V1\dariu <robertsond3@vcu.edu>
2020-09-24 20:41:45 -0400

    Main change is the background color

---
+++ app/src/main/java/com/example/teama/IngredientsActivity.java
@@ -0,0 +1,16 @@
+package com.example.teama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientsActivity extends AppCompatActivity {
+
+    //Class needs a database of ingredients added.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients);
+    }
+}
--- app/src/main/java/com/example/teama/MainActivity.java
+++ app/src/main/java/com/example/teama/MainActivity.java
@@ -43,7 +43,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //mealPrep button to go to calendar view
+        Button ingredients = (Button)findViewById(R.id.buttonIngredients);
+        ingredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,IngredientsActivity.class));
+            }
+        });
+
 
     }
 }
