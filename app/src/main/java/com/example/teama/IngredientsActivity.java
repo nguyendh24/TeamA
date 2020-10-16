6b0ba5d8f9995e60728f7bbb708181e0620660c4
Diana Nguyen <>
2020-10-15 23:28:14 -0400

    small layout changes

--- app/src/main/java/com/example/teama/IngredientsActivity.java
+++ app/src/main/java/com/example/teama/IngredientsActivity.java
@@ -4,24 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class IngredientsActivity extends AppCompatActivity {
     private SearchView svIngredients;
-    private DatabaseHelper db;
+
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
-        db = new DatabaseHelper(this);
 
-        svIngredients = findViewById(R.id.searchView);
-        svIngredients.setOnClickListener(new View.OnClickListener() {
+
+        /**********************************************************
+         * DELETE
+         */
+        name = findViewById(R.id.textView5);
+        final Button nameButton = findViewById(R.id.button3);
+        nameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                name.setText("Diana Nguyen");
             }
         });
+        /**
+         * DELETE
+         **********************************************************/
 
 
 
