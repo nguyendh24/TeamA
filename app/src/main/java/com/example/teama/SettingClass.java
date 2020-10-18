5c93ed406d3ce42485b69251b333a495b919eeb1
Diana Nguyen <>
2020-10-17 23:59:41 -0400

    Changing Settings to Diets

--- app/src/main/java/com/example/teama/SettingClass.java
+++ app/src/main/java/com/example/teama/SettingClass.java
@@ -1,14 +1,54 @@
 package com.example.teama;
+/**
+ * Changing this button from settings to dietary restrictions that user
+ * can specify and will populate appropriate recipes accordingly ?
+ */
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SettingClass extends AppCompatActivity {
+    private SearchView svDiets;
+    private ListView myDietList;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        svDiets = (SearchView) findViewById(R.id.searchView);
+        myDietList = (ListView) findViewById(R.id.);
+        list = new ArrayList<>();
+
+        list.add("Keto");
+        list.add("Low-carb");
+        list.add("Paleo");
+        list.add("Vegan");
+        list.add("Atkins");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        myDietList.setAdapter(adapter);
+
+        svDiets.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+
+                return false;
+            }
+        });
     }
 }
