ea852ba82a1c4ccff43f662b1630aa73e057c287
Diana Nguyen <>
2020-10-31 00:44:27 -0400

    Espresso test failed in studio because of animation but passes in github? Tried to edit code but still getting RunTimeException

--- app/src/androidTest/java/com/example/teama/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/teama/LoginActivityTest.java
@@ -1,19 +1,19 @@
 package com.example.teama;
 
+import android.content.ComponentName;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
-
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
 
 public class LoginActivityTest {
     @Rule
@@ -34,9 +34,14 @@ public class LoginActivityTest {
         Espresso.onView(withId(R.id.editTextEmailAddress)).perform(typeText(username));
         //close keyboard
         Espresso.closeSoftKeyboard();
+        //input password into password field
+        Espresso.onView(withId(R.id.editTextRegPW)).perform(typeText(password));
+        //close keyboard
+        Espresso.closeSoftKeyboard();
         //click login button
         Espresso.onView(withId(R.id.buttonLogin)).perform(click());
         //checking if it started new intent
+        intended(hasComponent(new ComponentName(getApplicationContext(), MainActivity.class)));
 
     }
     @After
