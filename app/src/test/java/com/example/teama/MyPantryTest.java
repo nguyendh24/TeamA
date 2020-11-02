9527f1bdb5e56624cfa626f2d03d24e1081595a3
Diana Nguyen <>
2020-11-02 13:15:59 -0500

    Had failed local unit test -- fixed it should fix build

--- app/src/test/java/com/example/teama/MyPantryTest.java
+++ app/src/test/java/com/example/teama/MyPantryTest.java
@@ -54,10 +54,17 @@ public class MyPantryTest extends TestCase {
     }
      */
     @Test
-    public void testToString() {
+    public void testToString_true() {
         itemTest = new Pantry_List("apple", 2);
-        assertEquals(String.format("%-15s %s%d", "orange","x ", 2), itemTest.toString());
+        assertEquals(String.format("%-25s %s%d", "apple","x ", 2), itemTest.toString());
     }
+
+    @Test
+    public void testToString_false() {
+        itemTest = new Pantry_List("apple", 2);
+        assertNotSame(String.format("%-25s %s%d", "orange","x ", 2), itemTest.toString());
+    }
+
     @Test
     public void testRandomMethodBecauseICantFigureOutUnitTestForOtherOne() {
         test = new MyPantry();
